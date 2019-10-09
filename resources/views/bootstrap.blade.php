303cf1109fa236216464a3d2124251f0fe55275f
deddew <deddew@hotmail.com>
2019-10-09 17:42:23 +0200

    first commit

---
+++ resources/views/bootstrap.blade.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <!-- <meta name="viewport" content="width=device-width, initial-scale=1"> -->
+        <link rel="stylesheet" href="css/app.css">
+    </head>
+    <title>Bootstrap</title>
+    <body>
+<div class="container">
+
+<div class="row">
+<div class="col">
+<h2>Bloembinderij de Tulp</h2>
+<a href="http://localhost:8000/bootstrap2grid">Bloemen</a>
+<a href="#">Boeketten</a>
+<a href="#">Winkel</a>
+<a href="http://localhost:8000/javas">Over ons</a>
+</div>
+</div>
+
+<div class="row">
+<div class="col">
+<div class="jumbotron">
+<p class="lead">"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+</p>
+<hr/><p>Neem contact met ons op of bezoek de website</p>
+<p class="lead">
+<a href="#" class="btn btn-primary btn-lg" role="button">contact</a>
+
+</p>
+</div>
+</div>
+</div>
+
+
+<div class="row">
+    <div class="col">
+    <h3>Trouwboeketten</h3>
+    <img src="images/3.jpg" alt="trouwboeket" class='img-fluid'>
+    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
+    </div> 
+
+    <div class="col">
+    <h3>verjaardag</h3>
+    <img src="images/2.jpg" alt="verjaardag" class='img-fluid'>
+    It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.
+    </div> 
+
+    <div class="col">
+    <h3>Jubileum</h3>
+    <img src="images/1.jpg" alt="jubileum" class='img-fluid'>
+    It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable.
+    </div> 
+</div>
+
+<div class="row">
+<div class="col-md-8 offset-2">
+<div class="blue">
+<h2>gecentreerd</h2>
+</div>
+</div>
+</div>
+
+
+<div class="row">
+<div class="col">
+<hr/>
+<p class="text-center"> 
+Voettekst Copyright
+</p>
+</div>
+</div>
+
+
+
+</div> 
+<script src="js/app.js"></script> 
+   </body>
+</html>
---
+++ resources/views/bootstrap2grid.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <!-- <meta name="viewport" content="width=device-width, initial-scale=1"> -->
+        <link rel="stylesheet" href="css/app.css">
+    </head>
+    <title>Bootstrap</title>
+    <body>
+<div class="container">
+
+<div class="row">
+<div class="col">
+<h2>Bloembinderij de Tulp</h2>
+<a href="http://localhost:8000/bootstrap">Bloemen</a>
+<a href="#">Boeketten</a>
+<a href="#">Winkel</a>
+<a href="http://localhost:8000/javas">Over ons</a>
+</div>
+</div>
+
+
+
+
+<div class="row">
+    <div class="col-md-4">
+    <h3>Trouwboeketten</h3>
+    
+    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
+    </div> 
+
+    <div class="col-md-4">
+    <h3>verjaardag</h3>
+    
+    It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.
+    </div> 
+
+    <div class="col-md-4">
+    <h3>Jubileum</h3>
+    
+    It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable.
+    
+    
+    
+    </div> 
+</div>
+
+<div class="row">
+<div class="col-md-8 offset-2">
+<div class="bg-primary">
+<h2>gecentreerd</h2>
+</div>
+</div>
+</div>
+
+<div class="row">
+<div class="col-md-4">
+<div class="bg-primary">
+<h2>gecentreerd</h2>
+</div>
+</div>
+<div class="col-md-4 offset-4">
+<div class="bg-primary">
+<h2>gecentreerd</h2>
+</div>
+</div>
+</div>
+
+
+
+
+
+
+<div class="row">
+<div class="col">
+<hr/>
+<p class="text-center"> 
+Voettekst Copyright
+</p>
+</div>
+</div>
+
+
+
+</div> 
+<script src="js/app.js"></script> 
+   </body>
+</html>
